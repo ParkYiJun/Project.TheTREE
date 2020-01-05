6a8102ce28a101c1d9373601520a11da8eab4462
ParkYiJun <ydlwnsj25@gmail.com>
2020-01-05 21:32:13 +0900

    BaseProject

---
+++ Source/TheTree/TheTreeGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TheTreeGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class THETREE_API ATheTreeGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
